4024e475088dbf48ac1d03ee79dd46de81d37b34
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-10-02 12:57:41 +0100

    Redis connection

---
+++ utils/redis.js
@@ -0,0 +1,31 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor () {
+    this.client = createClient();
+    this.client.on('error', (error) => console.log(error); );
+    this.connected = false;
+    this.connected = this.client.connec();
+  }
+  
+  isAlive() {
+      if (this.connected) {
+        return true;
+      } else {
+        return false;
+      }
+  }
+
+  async get(key) {
+    const value = await this.client.get(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    await this.client.set(key, value, { EX: duration });
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
